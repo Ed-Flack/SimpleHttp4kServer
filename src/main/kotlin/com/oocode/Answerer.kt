63b8dda67515731abe65bee968dec00f07ea65a2
Ed <mredwardflack@gmail.com>
2025-04-07 14:51:29 +0100

    answers with my name

--- src/main/kotlin/com/oocode/Answerer.kt
+++ src/main/kotlin/com/oocode/Answerer.kt
@@ -3,7 +3,7 @@ package com.oocode
 class Answerer {
     fun answerFor(question: String): String {
         if (question.contains("name")) {
-            return "Someone"
+            return "Ed and Ilona"
         }
         if (question.contains("2")) {
             return "4"
