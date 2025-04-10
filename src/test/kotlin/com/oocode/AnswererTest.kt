b2c3013df0f041cc0d6f0b955bf4dd9b763f7e23
Ed <mredwardflack@gmail.com>
2025-04-10 14:52:45 +0100

    update tests

--- src/test/kotlin/com/oocode/AnswererTest.kt
+++ src/test/kotlin/com/oocode/AnswererTest.kt
@@ -8,7 +8,7 @@ class AnswererTest {
     @Test
     fun `can answer name`() {
         val contents = Answerer().answerFor("What is your name?")
-        assertThat(contents, equalTo("Ed and Ilona"))
+        assertThat(contents, equalTo("Flack Attack"))
     }
 
     @Test
